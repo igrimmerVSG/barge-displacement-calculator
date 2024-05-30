2a382d874e4bdedef20cb32e5dbf3ffb2d63b31a
igrimmerVSG <igrimmer@vsg.ca>
2024-05-30 20:24:24 +0000

    lets get it

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,12 @@ import pandas as pd
 import streamlit as st
 import datetime
 
+st.logo('Logo.png',icon_image='Logo.png')
+
+st.image('Logo.png')
 """
-# Vanc!
+## Barge Displacement Calculator
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 r1c1,r1c2 = st.columns(2)
 
@@ -21,7 +19,7 @@ with r1c1:
     st.write("Barge loaded on:", date)
 
 with r1c2:
-    barge = st.selectbox("Which barge was loaded?",("MLT4000-2","MLT 4000-4","MLT 4000-5 / MLT 4000-6"))
+    barge = st.selectbox("Which barge was loaded?",("MLT 4000-2","MLT 4000-4","MLT 4000-5 / MLT 4000-6"))
     st.write("Barge selected: ", barge)
 
 r2c1,r2c2 = st.columns(2)
@@ -35,7 +33,7 @@ with r2c1:
         }
     )
 
-    st.data_editor(
+    emptyInputEdit = st.data_editor(
         emptyInput,
         column_config={
             'eloc': 'Measured from: ',
@@ -57,6 +55,10 @@ with r2c1:
             )
         }
     )
+    emptySumFt=emptyInputEdit['eft'].sum()
+    emptySumIn=emptyInputEdit['ein'].sum()
+    emptyAvgM=(emptySumFt+emptySumIn/12)*0.3048/4
+    st.write('Average empty freeboard (meters):',emptyAvgM)
 with r2c2:
     loadedInput = pd.DataFrame(
         {
@@ -66,7 +68,7 @@ with r2c2:
         }
     )
 
-    st.data_editor(
+    loadedInputEdit = st.data_editor(
         loadedInput,
         column_config={
             'lloc': 'Measured from: ',
@@ -88,3 +90,33 @@ with r2c2:
             )
         }
     )
+    loadedSumFt=loadedInputEdit['lft'].sum()
+    loadedSumIn=loadedInputEdit['lin'].sum()
+    loadedAvgM=(loadedSumFt+loadedSumIn/12)*0.3048/4
+    st.write('Average loaded freeboard (meters):',loadedAvgM)
+
+def bargeInterpolate(barge):
+    match barge:
+        case 'MLT 4000-2':
+            displacementChart=pd.read_csv('MLT 4000-2.csv')
+            emptyDisplacement=displacementChart.iloc[(displacementChart['Freeboard Mean (m)']-emptyAvgM).abs().argsort()[:2]]
+            loadedDisplacement=displacementChart.iloc[(displacementChart['Freeboard Mean (m)']-loadedAvgM).abs().argsort()[:2]]
+            with r2c1: st.write(emptyDisplacement)
+            with r2c2: st.write(loadedDisplacement)
+        case 'MLT 4000-4':
+            displacementChart=pd.read_csv('MLT 4000-4.csv')
+            emptyDisplacement=displacementChart.iloc[(displacementChart['Freeboard Mean (m)']-emptyAvgM).abs().argsort()[:2]]
+            loadedDisplacement=displacementChart.iloc[(displacementChart['Freeboard Mean (m)']-loadedAvgM).abs().argsort()[:2]]
+            with r2c1: st.write(emptyDisplacement)
+            with r2c2: st.write(loadedDisplacement)
+        case 'MLT 4000-5 / MLT 4000-6':
+            displacementChart=pd.read_csv('MLT 4000-5 & MLT 4000-6.csv')
+            if emptyAvgM>displacementChart['Freeboard Mean (m)'].max():
+                emptyDisplacement=793
+            else:
+                emptyDisplacement=displacementChart.iloc[(displacementChart['Freeboard Mean (m)']-emptyAvgM).abs().argsort()[:2]]
+            loadedDisplacement=displacementChart.iloc[(displacementChart['Freeboard Mean (m)']-loadedAvgM).abs().argsort()[:2]]
+            with r2c1: st.write(emptyDisplacement)
+            with r2c2: st.write(loadedDisplacement)
+
+bargeInterpolate(barge)
